b9af41c0bde312a33bec5a1d132d88377f4486e2
Metin Yıldıran <metin.1464@gmail.com>
2022-03-01 11:46:02 +0300

    Implemented persistent data logic

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    // Singleton
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        LoadColor();
+    }
+
+    [Serializable]
+    private class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        var saveData = new SaveData();
+        saveData.TeamColor = TeamColor;
+
+        var json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        var path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            var json = File.ReadAllText(path);
+            var saveData = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = saveData.TeamColor;
+        }
+    }
+}
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -14,14 +14,16 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -36,5 +38,18 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit(0);
 #endif
+
+        MainManager.Instance.SaveColor();
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
